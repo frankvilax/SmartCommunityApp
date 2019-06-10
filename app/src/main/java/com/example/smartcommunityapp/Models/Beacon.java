bb777f554e4ba1e7a9ffe5bc625944b778b5bf82
frankvilax <frankvilax@gmail.com>
2019-06-10 23:40:34 +0100

    Beacon Model updated

--- app/src/main/java/com/example/smartcommunityapp/Models/Beacon.java
+++ app/src/main/java/com/example/smartcommunityapp/Models/Beacon.java
@@ -1,4 +1,40 @@
 package com.example.smartcommunityapp.Models;
 
 public class Beacon {
+    private String _uid;
+    private String _location;
+    private boolean _isActive;
+
+    public Beacon(String uid, String location, boolean isActive){
+        _uid = uid;
+        _location = location;
+        _isActive = isActive;
+    }
+
+    // Getters
+    public String getUid(){
+        return _uid;
+    }
+
+    public String getLocation(){
+        return _location;
+    }
+
+    public boolean isActive(){
+        return _isActive;
+    }
+
+    // Setters
+
+    public void setUid(String uid){
+        _uid = uid;
+    }
+
+    public void setLocation(String location){
+        _location = location;
+    }
+
+    public void setIsActive(boolean isActive){
+        _isActive = isActive;
+    }
 }
